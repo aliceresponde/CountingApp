014eb3483e0b12c1e98d570276041d284edab629
Alicia Beltran <aliceresponde@gmail.com>
2020-08-02 05:15:36 -0500

    setup project settings and structure

---
+++ app/src/main/java/com/aliceresponde/countingapp/presentation/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.aliceresponde.countingapp.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.aliceresponde.countingapp.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
