64bad52cd38c6ee24bd7b08fac1db3c1778de27f
Alicia Beltran <aliceresponde@gmail.com>
2020-08-02 06:25:14 -0500

    create WelcomeFragment

--- app/src/main/java/com/aliceresponde/countingapp/presentation/MainActivity.kt
+++ app/src/main/java/com/aliceresponde/countingapp/presentation/MainActivity.kt
@@ -3,10 +3,12 @@ package com.aliceresponde.countingapp.presentation
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.aliceresponde.countingapp.R
+import com.aliceresponde.countingapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
